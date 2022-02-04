16e714e83e59aac86af3103d598f3d5706734009
RafliFerdian25 <ferdianrafli25@gmail.com>
2022-02-04 11:31:35 +0700

    soal-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,98 @@ $(document).ready(function () {
   $("#tabel__sekolah").DataTable({
     pageLength: 5,
   });
+  $("#tabel__uts").DataTable({
+    pageLength: 10,
+  });
+  $("#tabel__uas").DataTable({
+    pageLength: 10,
+  });
 });
+// ========== owl carousel ==========
+// index - Beranda
+$(document).ready(function () {
+  $(".owl-carousel#beranda__rekap").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
+
+// NEW
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+// mapel
+$(".tambah__materi").hover(
+  function () {
+    $(".fa-plus").css("color", "white");
+  },
+  function () {
+    $(".fa-plus").css("color", "black");
+  }
+);
+
+// Tambah materi
+var i = 4;
+var j = 5;
+function tambah__materi() {
+  $("#" + i + "_materi").after(
+    `<div class="mb-3 row" id="` +
+      j +
+      `_materi">
+      <label for="materi_` +
+      j +
+      `" class="col-md-2 text-md-end col-form-label">Materi ` +
+      j +
+      `</label>
+        <div class="col-lg-9 col-md-10">
+          <input type="text" name="materi_` +
+      j +
+      `" class="form-control rounded-3" id="materi_` +
+      j +
+      `">
+      </div>
+    </div>
+    <!-- end materi ` +
+      j +
+      ` -->`
+  );
+  i++;
+  j++;
+}
